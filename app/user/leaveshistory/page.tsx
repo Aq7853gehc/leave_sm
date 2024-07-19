9d5695b8cb8a6bf11b686b93cb3794835bb2d877
Aq7853gehc <aquib7583@gmail.com>
2024-07-19 14:45:14 +0530

    some changes

--- app/user/leaveshistory/page.tsx
+++ app/user/leaveshistory/page.tsx
@@ -9,11 +9,17 @@ export default async function DemoPage() {
   const approve = data.filter((d) => d.status === "approved");
   return (
     <div className="container mx-auto py-10 min-h-screen ">
-      <div className="flex justify-between">
-        <h1 className="text-4xl font-bold ">Leave History</h1>
-        <h2 className="text-2xl font-semibold">Balance: <span className="text-2xl font-medium">43 days</span></h2>
+      <div className="flex items-center justify-between">
+        <div>
+          <h1 className="text-4xl font-bold ">Leave History</h1>
+          <p className="font-medium text-base text-gray-400">
+            This page is take the history of last years{" "}
+          </p>
+        </div>
+        <h2 className="text-2xl font-semibold">
+          Balance: <span className="text-2xl font-medium">43 days</span>
+        </h2>
       </div>
-      <p className="font-medium text-base text-gray-400">This page is take the history of last years </p>
       <Separator className="mb-5 mt-2" />
       <Tabs defaultValue="all">
         <TabsList>
