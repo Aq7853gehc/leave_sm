b3617ce9eefb3f5006f89aca367db13fde6d1381
Aq7853gehc <aquib7583@gmail.com>
2024-07-19 09:53:46 +0530

    NEW

--- app/user/leaveshistory/page.tsx
+++ app/user/leaveshistory/page.tsx
@@ -8,11 +8,11 @@ export default async function DemoPage() {
   const data = await getData();
   return (
     <div className="container mx-auto py-10 min-h-screen ">
-      <div className="flex justify-between">
+      <div className="flex  flex-col justify-between">
         <h1 className="text-4xl font-bold ">Leave History</h1>
-        <h2 className="text-2xl font-semibold">Balance: <span className="text-2xl font-medium">43 days</span></h2>
-      </div>
       <p className="font-medium text-base text-gray-400">This page is take the history of last years </p>
+      </div>
+        <h2 className="text-2xl font-semibold">Balance: <span className="text-2xl font-medium">43 days</span></h2>
       <Separator className="mb-5 mt-2" />
       <Tabs defaultValue="all">
         <TabsList>
