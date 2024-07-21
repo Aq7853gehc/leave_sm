c9ef6e06a14c3ebaff02fdbe5b4c014dc7832783
Aq7853gehc <aquib7583@gmail.com>
2024-07-21 19:00:16 +0530

    new commit

--- app/user/leavebalance/page.tsx
+++ app/user/leavebalance/page.tsx
@@ -1,8 +1,19 @@
+import { Separator } from '@/components/ui/separator'
 import React from 'react'
 
 const LeaveBalance = () => {
   return (
-    <div>Balance</div>
+    <div className="flex flex-col w-full">
+      <h1 className="text-4xl font-black">Leaves Balance</h1>
+      <p className="text-base text-gray-500">
+        Here you make changes according to your prefrence
+      </p>
+      <Separator className="my-2" />
+      <div>
+        {/* Settings below */}
+      
+      </div>
+    </div>
   )
 }
 
--- app/user/leavesrequest/page.tsx
+++ app/user/leavesrequest/page.tsx
@@ -6,7 +6,7 @@ import React from "react";
 
 const LeaveRequest = () => {
   return (
-    <div className="h-screen w-full flex  flex-col ">
+    <div className="h-screen w-full flex  flex-col overflow-scroll">
       <h1 className="text-4xl font-bold ">Leaves Request</h1>
       <p className="text-base text-gray-500">Here apply for leaves</p>
       <Separator className="mb-1" />
@@ -49,12 +49,7 @@ const LeaveRequest = () => {
                   >
                     Start Date
                   </label>
-                  {/* <input
-                    type="date"
-                    name="date"
-                    id="date"
-                    className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-3 text-base font-medium text-[#6B7280] outline-none focus:border-blue-600 focus:shadow-md"
-                  /> */}
+
                   <Input type="date" />
                 </div>
               </div>
@@ -66,12 +61,7 @@ const LeaveRequest = () => {
                   >
                     End Date
                   </label>
-                  {/* <input
-                    type="date"
-                    name="date"
-                    id="date"
-                    className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-blue-600 focus:shadow-md"
-                  /> */}
+
                   <Input type="date" />
                 </div>
               </div>
@@ -83,13 +73,7 @@ const LeaveRequest = () => {
               >
                 Alternate Phone Number
               </label>
-              {/* <input
-                type="text"
-                name="phone"
-                id="phone"
-                placeholder="Enter your phone number"
-                className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-blue-600 focus:shadow-md"
-              /> */}
+
               <Input
                 placeholder="Enter your phone number"
                 type={"tel"}
@@ -103,13 +87,7 @@ const LeaveRequest = () => {
               >
                 Alternate Email Address
               </label>
-              {/* <input
-                type="email"
-                name="email"
-                id="email"
-                placeholder="Enter your email"
-                className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-blue-600 focus:shadow-md"
-              /> */}
+
               <Input type="email" placeholder="Enter your email" required />
             </div>
             <div className="mb-2 pt-3">
@@ -118,33 +96,19 @@ const LeaveRequest = () => {
               </label>
               <div className=" flex flex-wrap">
                 <div className="w-full sm:w-full">
-                  {/* <textarea
-                    placeholder="Reason...."
-                    className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-2 text-base font-medium text-[#6B7280] outline-none focus:border-blue-600 focus:shadow-md"
-                  /> */}
                   <Textarea placeholder="Type reason here..." />
                 </div>
               </div>
             </div>
 
-            <div className="flex flex-col gap-5">
-              {/* <div>
-                {/* <button className="hover:bg-transparent transition-all hover:text-black hover:border-2 hover:border-gray-400 w-full rounded-md bg-blue-600 py-3 px-8 text-center text-base font-semibold text-white outline-none">
-                  Submit Request
-                </button> */}
-
-              <div className="flex justify-around">
-                {/* <button className="hover:shadow-lg  w-full transition-all rounded-md py-3 px-8 text-center text-base border-2 font-semibold border-gray-400">
-                  Cancel Request
-                  </button> */}
-                <Button variant={"destructive"}>Cancel Request</Button>
-                <Button className="bg-blue-500 hover:bg-blue-600 ">
-                  Submit Request
-                </Button>
-                <Button variant={"outline"} className="">
-                  Save as Draft
-                </Button>
-              </div>
+            <div className="flex  justify-around">
+              <Button variant={"destructive"}>Cancel Request</Button>
+              <Button className="bg-blue-500 hover:bg-blue-600 ">
+                Submit Request
+              </Button>
+              <Button variant={"outline"} className="">
+                Save as Draft
+              </Button>
             </div>
           </form>
         </div>
